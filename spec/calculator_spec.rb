ab7860109372907fd87993f24e30a6ab7683c1fa
Michael Young <mikeyoung13@gmail.com>
2011-02-12 14:55:14 -0500

    formatted test code and added .rvmrc

--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -2,52 +2,54 @@ require 'rspec'
 require_relative '../lib/calculator.rb'
 
 describe Calculator do
-  
+
   before(:each) do
     @calculator = Calculator.new
   end
-  
+
   it "should be Calculator" do
     @calculator.should be_a(Calculator);
   end
-  
+
   subject {@calculator}
   specify { @calculator.should be_a(Calculator)}
-  
+
   context "handling addition with" do
-  
-  it "two numbers2" do
-    #pending ("Addition") do
+
+    it "two numbers2" do
+      #pending ("Addition") do
       subject.add(5,2).should == 7
-    #end
-  end  
-  
-  it "two numbers" do
-    #pending "Addition"
-    subject.add(5,2).should == 7
-  end
-  
-  it "positive and negative number" do
-    subject.add(7,-2).should == 5
-  end
-  
-  it "two negative numbers" do
-    subject.add(-2,-3).should == -5
-  end
-  
-  it "three numbers" do
-    subject.add(2,3,4).should == 9
-  end
-  
-  it "five numbers" do
-    subject.add(2,3,4,5,6).should == 20
-  end  
-  end
-    context "handling subtraction with" do
-      
-      it "two numbers" do
-        subject.subtract(5,2).should == 3
-      end
+      #end
+    end  
+
+    it "two numbers" do
+      #pending "Addition"
+      subject.add(5,2).should == 7
     end
+
+    it "positive and negative number" do
+      subject.add(7,-2).should == 5
+    end
+
+    it "two negative numbers" do
+      subject.add(-2,-3).should == -5
+    end
+
+    it "three numbers" do
+      subject.add(2,3,4).should == 9
+    end
+
+    it "five numbers" do
+      subject.add(2,3,4,5,6).should == 20
+    end  
+  end
   
+  
+  context "handling subtraction with" do
+
+    it "two numbers" do
+      subject.subtract(5,2).should == 3
+    end
+  end
+
 end
