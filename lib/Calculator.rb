1b8413dde15e39f05313b486bb1e784aaeb4ef1c
Michael Young <mikeyoung13@gmail.com>
2011-02-12 12:12:32 -0500

    support any numnber of args

--- lib/Calculator.rb
+++ lib/Calculator.rb
@@ -3,6 +3,14 @@ class Calculator
     val1 + val2 + val3
   end
   
+  def add (*args) 
+    sum = 0
+    args.each do | arg |
+      sum += arg
+    end
+    sum
+  end
+  
 #  def add(val1,val2,val3)
 #    val1 + val2 + val3
 #  end
