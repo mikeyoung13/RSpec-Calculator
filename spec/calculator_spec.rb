9b507a0493bb965ee19f72e45432c72cc49eb1c2
Michael Young <mikeyoung13@gmail.com>
2011-02-12 12:05:20 -0500

    refactor with subject cond context

--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -11,21 +11,26 @@ describe Calculator do
     @calculator.should be_a(Calculator);
   end
   
+  subject {@calculator}
   specify { @calculator.should be_a(Calculator)}
   
-  it "should add two numbers2" do
+  context "handling addition with" do
+  
+  it "two numbers2" do
     #pending ("Addition") do
-      @calculator.add(5,2).should == 7
+      subject.add(5,2).should == 7
     #end
   end  
   
-  it "should add two numbers" do
+  it "two numbers" do
     #pending "Addition"
-    @calculator.add(5,2).should == 7
+    subject.add(5,2).should == 7
   end
   
-  it "should add positive and negative number" do
-    @calculator.add(7,-2).should == 5
+  it "positive and negative number" do
+    subject.add(7,-2).should == 5
   end  
+  
+end
   
 end
